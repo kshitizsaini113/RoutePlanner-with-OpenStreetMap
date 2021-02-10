86ba57fd5f44cbeb36e724f3d6fe44d9688a6682
kshitizsaini113 <kshitizsaini.rtk@gmail.com>
2021-02-11 00:30:03 +0530

    Initializing a vector to create the board.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RoutePlanner-with-OpenStreetMap
+
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<std::vector<int>> board = {{0, 1, 0, 0, 0, 0},
+                                           {0, 1, 0, 0, 0, 0},
+                                           {0, 1, 0, 0, 0, 0},
+                                           {0, 1, 0, 0, 0, 0},
+                                           {0, 0, 0, 0, 1, 0}};
+    // Creating a board in the form of array.
+
+    std::cout << "Hello!" << "\n";
+    // Debugging by printing hello message.
+
+    return 0;
+}
