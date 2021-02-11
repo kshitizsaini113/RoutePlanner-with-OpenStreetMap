c139fb7aec7f35de29ed961bf93c9a1aa723233d
kshitizsaini113 <kshitizsaini.rtk@gmail.com>
2021-02-11 08:11:29 +0530

    Added PrintBoard functionality.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include <vector>
 
+void PrintBoard( std::vector<std::vector<int>> board )
+{
+// Takes the board as the argument for the printBoard function  and prints the board.
+
+    std::cout<<std::endl;
+    // Prints a new-line to make the formatting better.
+
+    for( auto row: board )
+    {
+    // Access the rows from the board.
+        for( auto element: row )
+        {
+        // Access the element from a particular row.
+            std::cout<<element<<"  ";
+            // Prints the element of each row.
+        }
+        std::cout<<std::endl;
+        // Prints a new line to access the data of a row in a single line.
+    }
+}
+
 int main()
 {
     std::vector<std::vector<int>> board = {{0, 1, 0, 0, 0, 0},
@@ -8,10 +29,10 @@ int main()
                                            {0, 1, 0, 0, 0, 0},
                                            {0, 1, 0, 0, 0, 0},
                                            {0, 0, 0, 0, 1, 0}};
-    // Creating a board in the form of array.
+    // Creating a board in the form of vector.
 
-    std::cout << "Hello!" << "\n";
-    // Debugging by printing hello message.
+    PrintBoard(board);
+    // Debugging by printing the board from PrintBoard function.
 
     return 0;
 }
