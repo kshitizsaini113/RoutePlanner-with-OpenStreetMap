017d2d0f58383bdef717ec1889b74bc6592c6480
kshitizsaini113 <kshitizsaini.rtk@gmail.com>
2021-02-11 08:27:54 +0530

    Reading the board from file.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+
+std::vector<std::vector<int>> ReadBoardFile ( std::string boardPath )
+{
+// readBoardFile function supports the path of the file as argument, opens it, processes it and
+// returns a 2D Vector denoting a board.
+
+    std::vector<std::vector<int>> board;
+    // Creating an empty board inform of 2D-Vector to be read from file.
+
+    std::ifstream boardFile;
+    // File handler for opening a file.
+
+    std::string row;
+    // Initializing a new line to be read from the board file.
+
+    boardFile.open( boardPath );
+    // Opens the board file and process the file till it contains the lines. During the process, it
+    // initialises the board.
+
+    if( boardFile )
+    {
+    // Checks if the file exists.
+        while(getline(boardFile, row))
+        // Fetches a line from the file
+        {
+            std::cout<<row<<std::endl;
+        }
+    }
+}
 
 void PrintBoard( std::vector<std::vector<int>> board )
 {
@@ -24,15 +54,12 @@ void PrintBoard( std::vector<std::vector<int>> board )
 
 int main()
 {
-    std::vector<std::vector<int>> board = {{0, 1, 0, 0, 0, 0},
-                                           {0, 1, 0, 0, 0, 0},
-                                           {0, 1, 0, 0, 0, 0},
-                                           {0, 1, 0, 0, 0, 0},
-                                           {0, 0, 0, 0, 1, 0}};
-    // Creating a board in the form of vector.
+    std::string boardPath = "/home/kshitizsaini113/Git-Repo/RoutePlanner-with-OpenStreetMap/1.board";
 
-    PrintBoard(board);
-    // Debugging by printing the board from PrintBoard function.
+    std::vector<std::vector<int>> board = ReadBoardFile( boardPath );
+
+//    PrintBoard(board);
+//    // Debugging by printing the board from PrintBoard function.
 
     return 0;
 }
